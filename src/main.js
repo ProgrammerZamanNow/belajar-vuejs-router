098e3d9ee90d23be0626a747e7d7f61981d51ca2
khannedy <echo.khannedy@gmail.com>
2025-01-04 23:44:47 +0700

    Add Vue Router setup to main.js
    Integrated Vue Router into the application by importing necessary dependencies and initializing the router with an empty route configuration. Updated the app creation to use the router instance. This change prepares the app for future route management.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
+import {createRouter, createWebHistory} from "vue-router";
 
-createApp(App).mount('#app')
+const router = createRouter({
+  routes: [],
+  history: createWebHistory()
+})
+
+createApp(App).use(router).mount('#app')
