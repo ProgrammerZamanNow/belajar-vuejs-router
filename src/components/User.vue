bcec41505e44d0863325a55ce23016f9965de31c
khannedy <echo.khannedy@gmail.com>
2025-01-05 14:00:04 +0700

    Add navigation links to User component
    Added links for Profile, Order, and Wishlist within the User component using `RouterLink`. Styled the active link to appear bold and red for better user navigation clarity. This improves the usability and organization of user-related pages.

--- src/components/User.vue
+++ src/components/User.vue
@@ -4,11 +4,20 @@
 
 <template>
   <h1>User</h1>
-  <div>Put header here</div>
+
+  <ul>
+    <li><RouterLink to="/users/profile">Profile</RouterLink></li>
+    <li><RouterLink to="/users/order">Order</RouterLink></li>
+    <li><RouterLink to="/users/wishlist">Wishlist</RouterLink></li>
+  </ul>
+
   <RouterView/>
   <div>Put footer here</div>
 </template>
 
 <style scoped>
-
+.router-link-exact-active{
+  color: red;
+  font-weight: bold;
+}
 </style>
